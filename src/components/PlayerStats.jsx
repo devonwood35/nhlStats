51e9addb1be089baf6231a3e285378609fac762f
Devon Wood <wooddv35@gmail.com>
2019-10-02 20:00:46 -0400

    Added player bio and player stats

---
+++ src/components/PlayerStats.jsx
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class PlayerStats extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stats: [],
+      statArr: [],
+      currentYear: '20182019',
+      statYear: {}
+    };
+  }
+
+  componentDidMount() {
+    this.updateStats();
+  }
+
+  changeYear = (event) => {
+    this.updateStats(event.target.getAttribute('value'));
+  }
+
+  updateStats(newYear) {
+    let { currentYear } = this.state;
+    const { isGoalie, player } = this.props;
+
+    newYear ? currentYear = newYear : newYear = null; //eslint-disable-line
+
+    let years;
+    if (isGoalie) {
+      years = player.filter((year) => year.stat.evenSaves);
+      years.forEach((year, index) => {
+        if (year.sequenceNumber === 2) {
+          // eslint-disable-next-line
+          for (const ele in year.stat) {
+            years[index - 1].stat[ele] += year.stat[ele]; //eslint-disable-line
+          }
+          years.splice(index, 1);
+        }
+      });
+      this.setState({
+        stats: years,
+        statArr: [
+          'Games Played', 'Wins', 'Losses', 'OT', 'Shutouts', 'Save %', 'GAA', 'Shots Against', 'Saves', 'Goals Against'
+        ],
+        currentYear,
+        statYear: years.filter((year) => year.season === currentYear)
+      });
+    } else {
+      years = player.filter((year) => year.stat.blocked);
+      years.forEach((year, index) => {
+        if (year.sequenceNumber === 2) {
+          // eslint-disable-next-line
+          for (const ele in year.stat) {
+            years[index - 1].stat[ele] += year.stat[ele]; //eslint-disable-line
+          }
+          years.splice(index, 1);
+        }
+      });
+      this.setState({
+        stats: years,
+        statArr: [
+          'Games Played', 'Goals', 'Assists', 'Points', '+/-', 'Faceoff %', 'Shot %', 'PiM', 'Hits', 'Blocks'
+        ],
+        currentYear,
+        statYear: years.filter((year) => year.season === currentYear)
+      });
+    }
+  }
+
+  render() {
+    const { stats, statArr, statYear } = this.state;
+    const { isGoalie } = this.props;
+
+    if (!statYear[0]) {
+      return (<div>loading...</div>);
+    }
+
+    return (
+      <div>
+        <div className="paginate">
+          {stats.map((year) => (
+            <button type="button" key={year.season} onClick={this.changeYear} value={year.season}>
+              {year.season}
+            </button>
+          )).reverse()}
+        </div>
+        <div className="quad-section">
+          <div className="first-element">
+            <div className="list--title">{statArr[0]}</div>
+            <div className="list--title">{statArr[1]}</div>
+            <div className="list--title">{statArr[2]}</div>
+            <div className="list--title">{statArr[3]}</div>
+            <div className="list--title">{statArr[4]}</div>
+          </div>
+          <div className="second-element">
+            {isGoalie
+              ? (
+                <div>
+                  <div>{statYear[0].stat.games}</div>
+                  <div>{statYear[0].stat.wins}</div>
+                  <div>{statYear[0].stat.losses}</div>
+                  <div>{statYear[0].stat.ot}</div>
+                  <div>{statYear[0].stat.shutouts}</div>
+                </div>
+              )
+              : (
+                <div>
+                  <div>{statYear[0].stat.games}</div>
+                  <div>{statYear[0].stat.goals}</div>
+                  <div>{statYear[0].stat.assists}</div>
+                  <div>{statYear[0].stat.points}</div>
+                  <div>{statYear[0].stat.plusMinus}</div>
+                </div>
+              )}
+          </div>
+          <div className="third-element">
+            <div className="list--title">{statArr[5]}</div>
+            <div className="list--title">{statArr[6]}</div>
+            <div className="list--title">{statArr[7]}</div>
+            <div className="list--title">{statArr[8]}</div>
+            <div className="list--title">{statArr[9]}</div>
+          </div>
+          <div className="fourth-element">
+            {isGoalie
+              ? (
+                <div>
+                  <div>{statYear[0].stat.savePercentage}</div>
+                  <div>{statYear[0].stat.goalAgainstAverage}</div>
+                  <div>{statYear[0].stat.shotsAgainst}</div>
+                  <div>{statYear[0].stat.saves}</div>
+                  <div>{statYear[0].stat.goalsAgainst}</div>
+                </div>
+              )
+              : (
+                <div>
+                  <div>{`${statYear[0].stat.faceOffPct}%`}</div>
+                  <div>{`${statYear[0].stat.shotPct}%`}</div>
+                  <div>{statYear[0].stat.penaltyMinutes}</div>
+                  <div>{statYear[0].stat.hits}</div>
+                  <div>{statYear[0].stat.blocked}</div>
+                </div>
+              )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+PlayerStats.propTypes = ({
+  isGoalie: PropTypes.bool.isRequired,
+  player: PropTypes.array.isRequired //eslint-disable-line
+});
+
+export default withRouter(PlayerStats);
--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../utils/api';
+import PlayerStats from '../components/PlayerStats';
 
 class Player extends Component {
   constructor(props) {
@@ -20,35 +21,66 @@ class Player extends Component {
     });
   }
 
+  isCaptain = (state) => {
+    if (state.captain) {
+      return `${state.fullName} (C)`;
+    }
+    if (state.alternateCaptain) {
+      return `${state.fullName} (A)`;
+    }
+    return state.fullName;
+  }
+
   render() {
     const { player } = this.state;
-    if (!player.primaryPosition) { return (<div>loading...</div>)};
+    if (!player.primaryPosition) { return (<div>loading...</div>); }
 
     return (
-      <div>
-        <div className="half-section">
-          <div className="first-element">{player.fullName}</div>
-          <div className="second-element half-section">
-            <div className="first-element">
-              <div>Position</div>
-              <div>Number</div>
-              <div>Nationality</div>
-              <div>Birthday</div>
-              <div>Height</div>
-              <div>Weight</div>
-              <div>Age</div>
-            </div>
-            <div className="second-element">
-              <div>{player.primaryPosition.name}</div>
-              <div>{player.primaryNumber}</div>
-              <div>{player.birthCountry}</div>
-              <div>{player.birthDate}</div>
-              <div>{player.height}</div>
-              <div>{player.weight}</div>
-              <div>{player.currentAge}</div>
-            </div>
+      <div className="box-container">
+        <div className="header-section header-section--large">
+          {this.isCaptain(player)}
+        </div>
+        <div className="quad-section list">
+          <div className="first-element">
+            <div className="list--title">Current Team</div>
+            <div />
+          </div>
+          <div className="second-element">
+            <div className="list--title">Number</div>
+            <div>{`#${player.primaryNumber}`}</div>
+          </div>
+          <div className="third-element">
+            <div className="list--title">Position</div>
+            <div>{player.primaryPosition.name}</div>
+          </div>
+          <div className="fourth-element">
+            <div className="list--title">Shoots/Catches</div>
+            <div>{player.shootsCatches}</div>
           </div>
         </div>
+        <div className="quad-section list">
+          <div className="first-element">
+            <div className="list--title">Nationality</div>
+            <div>{player.birthCountry}</div>
+          </div>
+          <div className="second-element">
+            <div className="list--title">Age</div>
+            <div>{player.currentAge}</div>
+          </div>
+          <div className="third-element">
+            <div className="list--title">Height</div>
+            <div>{player.height}</div>
+          </div>
+          <div className="fourth-element">
+            <div className="list--title">Weight</div>
+            <div>{player.weight}</div>
+          </div>
+        </div>
+        <div className="header-section">
+          Stats
+        </div>
+        { /* eslint-disable-next-line */ }
+        <PlayerStats isGoalie={player.primaryPosition.code === 'G' ? true : false} player={player.stats[0].splits} />
       </div>
     );
   }
