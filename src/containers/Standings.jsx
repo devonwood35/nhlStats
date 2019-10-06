26abecde744981af4602268ad37c6cbd85c3d994
Devon Wood <wooddv35@gmail.com>
2019-10-05 21:01:15 -0400

    Some CSS added titles to stats on standings

--- src/containers/Standings.jsx
+++ src/containers/Standings.jsx
@@ -34,14 +34,78 @@ class Standings extends Component {
         <div className="header-section list align__left">
           Eastern Conference
         </div>
-        <div className="header-section header-section--small list list--title align__left">
-          Metropolitan
+        <div className="twelfth-section background--black">
+          <div className="second-element header-section header-section--small">
+            Metropolitan
+          </div>
+          <div className="third-element list--title center--grid">
+            GP
+          </div>
+          <div className="fourth-element list--title center--grid">
+            W
+          </div>
+          <div className="fifth-element list--title center--grid">
+            L
+          </div>
+          <div className="sixth-element list--title center--grid">
+            OT
+          </div>
+          <div className="seventh-element list--title center--grid">
+            P
+          </div>
+          <div className="eigth-element list--title center--grid">
+            ROW
+          </div>
+          <div className="ninth-element list--title center--grid">
+            GF
+          </div>
+          <div className="tenth-element list--title center--grid">
+            GA
+          </div>
+          <div className="eleventh-element list--title center--grid">
+            Diff
+          </div>
+          <div className="twelfth-element list--title center--grid">
+            Streak
+          </div>
         </div>
         {standings[0].teamRecords.map((team) => (
           <Division key={team.team.id} team={team} />
         ))}
-        <div className="header-section header-section--small list list--title align__left">
-          Atlantic
+        <div className="twelfth-section background--black">
+          <div className="second-element header-section header-section--small">
+            Atlantic
+          </div>
+          <div className="third-element list--title center--grid">
+            GP
+          </div>
+          <div className="fourth-element list--title center--grid">
+            W
+          </div>
+          <div className="fifth-element list--title center--grid">
+            L
+          </div>
+          <div className="sixth-element list--title center--grid">
+            OT
+          </div>
+          <div className="seventh-element list--title center--grid">
+            P
+          </div>
+          <div className="eigth-element list--title center--grid">
+            ROW
+          </div>
+          <div className="ninth-element list--title center--grid">
+            GF
+          </div>
+          <div className="tenth-element list--title center--grid">
+            GA
+          </div>
+          <div className="eleventh-element list--title center--grid">
+            Diff
+          </div>
+          <div className="twelfth-element list--title center--grid">
+            Streak
+          </div>
         </div>
         {standings[1].teamRecords.map((team) => (
           <Division key={team.team.id} team={team} />
@@ -49,14 +113,78 @@ class Standings extends Component {
         <div className="header-section list align__left">
           Western Conference
         </div>
-        <div className="header-section header-section--small list list--title align__left">
-          Central
+        <div className="twelfth-section background--black">
+          <div className="second-element header-section header-section--small">
+            Central
+          </div>
+          <div className="third-element list--title center--grid">
+            GP
+          </div>
+          <div className="fourth-element list--title center--grid">
+            W
+          </div>
+          <div className="fifth-element list--title center--grid">
+            L
+          </div>
+          <div className="sixth-element list--title center--grid">
+            OT
+          </div>
+          <div className="seventh-element list--title center--grid">
+            P
+          </div>
+          <div className="eigth-element list--title center--grid">
+            ROW
+          </div>
+          <div className="ninth-element list--title center--grid">
+            GF
+          </div>
+          <div className="tenth-element list--title center--grid">
+            GA
+          </div>
+          <div className="eleventh-element list--title center--grid">
+            Diff
+          </div>
+          <div className="twelfth-element list--title center--grid">
+            Streak
+          </div>
         </div>
         {standings[2].teamRecords.map((team) => (
           <Division key={team.team.id} team={team} />
         ))}
-        <div className="header-section header-section--small list list--title align__left">
-          Pacific
+        <div className="twelfth-section background--black">
+          <div className="second-element header-section header-section--small">
+            Pacific
+          </div>
+          <div className="third-element list--title center--grid">
+            GP
+          </div>
+          <div className="fourth-element list--title center--grid">
+            W
+          </div>
+          <div className="fifth-element list--title center--grid">
+            L
+          </div>
+          <div className="sixth-element list--title center--grid">
+            OT
+          </div>
+          <div className="seventh-element list--title center--grid">
+            P
+          </div>
+          <div className="eigth-element list--title center--grid">
+            ROW
+          </div>
+          <div className="ninth-element list--title center--grid">
+            GF
+          </div>
+          <div className="tenth-element list--title center--grid">
+            GA
+          </div>
+          <div className="eleventh-element list--title center--grid">
+            Diff
+          </div>
+          <div className="twelfth-element list--title center--grid">
+            Streak
+          </div>
         </div>
         {standings[3].teamRecords.map((team) => (
           <Division key={team.team.id} team={team} />
